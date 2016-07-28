b8f5bcace8795a645013418145a67775d4c22ff0
mark anthony valencia <valmarkencia@gmail.com>
2016-07-28 13:37:32 +0800

    site update
    update from under construction to complete

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	<div class="container mainpage">
 		<h1 class="brand">Mark Anthony Valencia</h1>
 		<h4 class="job-desc">Web designer | Front-End Developer | Web Developer</h4>
-		<span class="construct">Site is still in progress!</span>
 	</div>
 
 
